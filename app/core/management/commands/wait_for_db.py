07ce9e0670b61828f6c9a48d266e807a8b1b3d90
Vinicius Oliveira <vinioliveira.web@gmail.com>
2023-03-15 11:54:14 -0300

    :test_tube: test(core): add init test wait_for_db
    This commit adds an initial test for the wait_for_db management command that will be implemented using test driven development. The test ensures that the expected behavior of the feature is met once it's implemented.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,5 +8,6 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     """Django command to wait for database."""
 
-    def handle(self, *args: Any, **options: Any) -> Optional[str]:
+    # def handle(self, *args: Any, **options: Any) -> Optional[str]:
+    def handle(self, *args, **options):
         pass
