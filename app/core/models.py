2b8ae816633a04831735f8173f3883393eeedfdd
Vinicius Oliveira <vinioliveira.web@gmail.com>
2023-04-02 22:40:30 -0300

    :sparkles: feat(core): require email input
    This commit creates a test and implements a feature at the user manager
    to raise ValueError if email is not provided at user creation.

--- app/core/models.py
+++ app/core/models.py
@@ -14,6 +14,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
         normalized_email = self.normalize_email(email)
         user = self.model(email=normalized_email, **extra_fields)
         user.set_password(password)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -34,3 +34,8 @@ class ModelTests(TestCase):
         for email, expeted in sample_emails:
             user = get_user_model().objects.create_user(email, 'Sample123')
             self.assertEqual(user.email, expeted)
+
+    def test_new_user_without_email_reaises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
