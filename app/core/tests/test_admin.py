62e6676ab52b322bc211de8f9fec3b80c24ea398
Vinicius Oliveira <vinioliveira.web@gmail.com>
2023-05-01 23:53:32 -0300

    :sparkles: feat(core): add support creating users
    It creates a test and implements support creating users in the django
    admin area.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -17,7 +17,7 @@ class AdminSiteTests(TestCase):
             password='testpass123'
         )
         self.client.force_login(self.admin_user)
-        self.user=get_user_model().objects.create_user(
+        self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
             name='Test User'
@@ -30,10 +30,17 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
-    
+
     def test_edit_user_page(self):
         """Test the edit user page works."""
         url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
